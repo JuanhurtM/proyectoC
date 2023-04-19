28f8ad9582a673080019a9a8c67970a39619df28
JSCasalins <jcasalins@vortexbird.com>
2023-04-19 00:09:07 +0000

    .

---
+++ application.wsgi
@@ -0,0 +1,4 @@
+#!/usr/bin/pythoon
+import sys
+sys.path.insert(0,"/var/www/helloWorld/")
+from app import app as application
---
+++ hello.py
@@ -0,0 +1,7 @@
+#hello world
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'hello world'
